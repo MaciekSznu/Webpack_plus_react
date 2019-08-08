a06a0c8d0054f188d99ee09baa469a7ac4bd4044
Maciej Sznurawa <msznurawa@gmail.com>
2019-08-08 08:03:00 +0200

    Add webpack-dev-server & HMR/react-hot-loader

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import uuid from 'uuid';
 import style from './App.css';
 import Title from '../components/Title';
 import TodoTasks from '../components/TodoList';
+import { hot } from 'react-hot-loader';
 
 
 class App extends React.Component {
@@ -51,4 +52,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default hot(module)(App);
